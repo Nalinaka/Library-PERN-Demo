096e70f49ad2fe6f0c2ed6f30b7a695360ddd70e
Nalinaka <nalinaka@gmail.com>
2023-10-14 14:26:53 -0700

    AWS

--- server/server.js
+++ server/server.js
@@ -22,6 +22,10 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build/index.html'));
+// })
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../build/index.html')));
+}
